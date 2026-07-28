738a731a1101b22a54c151a46686432a121eb3a3
Anmol Dubey <anmoldubey2310@gmail.com>
2026-07-28 20:38:21 +0530

    Center loading screen vertically and horizontally with glowing card component

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -1,13 +1,14 @@
 import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router";
 import React from 'react'
+import LoadingScreen from "../../../components/LoadingScreen";
 
 const Protected = ({children}) => {
     const { loading,user } = useAuth()
 
 
     if(loading){
-        return (<main><h1>Loading...</h1></main>)
+        return <LoadingScreen message="Loading..." />
     }
 
     if(!user){
